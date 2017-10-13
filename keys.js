5cbc1de5156159ff2387909f85d87220d3e4b37f
Gene Williams <GeneWillb@Gmail.com>
2017-10-13 12:34:31 -0400

    working on twitter calls

--- keys.js
+++ keys.js
@@ -4,7 +4,7 @@ const twitterKeys = {
     consumer_key: 'isdCQo4V4slPMb4nSD1Q0eLK9',
     consumer_secret: 'O7iNIhkbSXCpoY8WKz6jNVjYzeJw8izmM3PeagUElPMSzlnU14',
     access_token_key: '918855201196597248-fZ9dBCITRsgX2zrmydm7EvpOXD4lcs5',
-    access_token_secret: '0S99QNmlMyztgz9Vsx99QFfUyugGrLrpdBeaxl6sfyUPS\n'
+    access_token_secret: '0S99QNmlMyztgz9Vsx99QFfUyugGrLrpdBeaxl6sfyUPS'
 };
 
 const spotifyKeys = {
--- liri.js
+++ liri.js
@@ -4,5 +4,53 @@ const twitter = require('twitter');
 const spotify = require('node-spotify-api');
 const request = require('request');
 
+let command = "";
+
+for (let i = 2; i < process.argv.length; i ++) {
+    command = command.trim() + " " + process.argv[i];
+    console.log(command);
+}
+
+switch (command) {
+    case "my tweets":
+        twitterFun();
+        break;
+    case "spotify this song":
+        spotifyFun();
+        break;
+    case "movie this":
+        OMDB();
+        break;
+    default:
+        console.log("Try one of these commands: my tweets, spotify this song, movie this");
+}
 
 
+function twitterFun() {
+    console.log("twitter was called");
+    const client = new twitter ({
+        consumer_key: keys.twitter.consumer_key,
+        consumer_secret: keys.twitter.consumer_secret,
+        access_token_key: keys.twitter.access_token_key,
+        access_token_secret: keys.twitter.access_token_secret
+    });
+    
+    const params = {screen_name: 'nodejs'};
+    
+    client.get('statuses/user_timeline', params, function(err, tweets, response) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(tweets);
+        // console.log(JSON.stringify(response));
+    })
+}
+
+function spotifyFun() {
+    console.log("spotify was called");
+}
+
+function OMDB() {
+    console.log("OMDB was called");
+
+}
