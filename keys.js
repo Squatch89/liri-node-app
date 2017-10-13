21d4c703e7df8c7d487f45023a00c980589c36ca
Gene Williams <GeneWillb@Gmail.com>
2017-10-13 11:38:29 -0400

    spotify keys and omdb keys added

--- keys.js
+++ keys.js
@@ -1,11 +1,22 @@
 console.log('this is loaded');
 
-var twitterKeys = {
+const twitterKeys = {
     consumer_key: 'isdCQo4V4slPMb4nSD1Q0eLK9',
     consumer_secret: 'O7iNIhkbSXCpoY8WKz6jNVjYzeJw8izmM3PeagUElPMSzlnU14',
     access_token_key: '918855201196597248-fZ9dBCITRsgX2zrmydm7EvpOXD4lcs5',
     access_token_secret: '0S99QNmlMyztgz9Vsx99QFfUyugGrLrpdBeaxl6sfyUPS\n'
 };
 
-module.exports = twitterKeys;
+const spotifyKeys = {
+    clientID: "351b03b19ef84ec1bf44ee97244753d8",
+    clientSecret: "cd3b38ce605340e6a799b0cd7113e243"
+};
+
+const OMDBKey = "40e9cece";
+
+module.exports = {
+    twitter: twitterKeys,
+    spotify: spotifyKeys,
+    OMDB: OMDBKey
+};
 
--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+const keys = require("./keys.js");
+
 const twitter = require('twitter');
 const spotify = require('node-spotify-api');
 const request = require('request');
