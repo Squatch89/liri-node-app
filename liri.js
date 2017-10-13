2061219633ebd57388c7385b66a40522ae095d1c
Gene Williams <GeneWillb@Gmail.com>
2017-10-13 14:34:20 -0400

    OMDB working making small final tweaks

--- liri.js
+++ liri.js
@@ -7,9 +7,9 @@ const request = require('request');
 const command = process.argv[2];
 let query = "";
 
-for (let i = 3; i < process.argv.length; i ++) {
+for (let i = 3; i < process.argv.length; i++) {
     query = query.trim() + " " + process.argv[i];
-    console.log(command);
+    console.log(query);
 }
 
 switch (command) {
@@ -19,17 +19,17 @@ switch (command) {
     case "spotify-this-song":
         spotifyFun();
         break;
-    case "movie this":
+    case "movie-this":
         OMDB();
         break;
     default:
-        console.log("Try one of these commands: my tweets, spotify this song, movie this");
+        console.log("Try one of these commands: my-tweets, spotify-this-song, movie-this");
 }
 
 
 function twitterFun() {
     console.log("twitter was called");
-    const client = new twitter ({
+    const client = new twitter({
         consumer_key: keys.twitter.consumer_key,
         consumer_secret: keys.twitter.consumer_secret,
         access_token_key: keys.twitter.access_token_key,
@@ -38,7 +38,7 @@ function twitterFun() {
     
     const params = {screen_name: 'TechManWallace'};
     
-    client.get('statuses/user_timeline', params, function(err, tweets, response) {
+    client.get('statuses/user_timeline', params, function (err, tweets, response) {
         if (err) {
             console.log(err);
         }
@@ -53,40 +53,82 @@ function twitterFun() {
             console.log("======================================================================");
             
         }
-        // console.log(JSON.stringify(response));
     })
 }
 
 function spotifyFun() {
     console.log("spotify was called");
-    const Spotify = new spotify ({
+    const Spotify = new spotify({
         id: keys.spotify.clientID,
         secret: keys.spotify.clientSecret
     });
-    Spotify.search({type: 'track', query: query, limit: 1}, function(err, data) {
-        if (err) {
-            console.log(err);
-        }
-        console.log("======================================================================");
-        console.log("\n");
-        console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
-        console.log(`Track Name: ${data.tracks.items[0].name}`);
-        console.log(`Album Name: ${data.tracks.items[0].album.name}`);
-        console.log(`Preview URL: ${data.tracks.items[0].preview_url}`);
-        console.log("\n");
-        console.log("======================================================================");
-        
-    // * Artist(s)
-    //
-    //     * The song's name
-    //
-    //     * A preview link of the song from Spotify
-    //
-    //     * The album that the song is from
-    })
+    if (query !== "") {
+        Spotify.search({type: 'track', query: query, limit: 1}, function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("======================================================================");
+            console.log("\n");
+            console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+            console.log(`Track Name: ${data.tracks.items[0].name}`);
+            console.log(`Album Name: ${data.tracks.items[0].album.name}`);
+            console.log(`Preview URL: ${data.tracks.items[0].preview_url}`);
+            console.log("\n");
+            console.log("======================================================================");
+        })
+    }
+    else {
+        Spotify.search({type: 'track', query: 'Ace of Base', limit: 1}, function (err, data) {
+            if (err) {
+                console.log(err);
+            }
+            console.log("======================================================================");
+            console.log("\n");
+            console.log(`Artist: ${data.tracks.items[0].artists[0].name}`);
+            console.log(`Track Name: ${data.tracks.items[0].name}`);
+            console.log(`Album Name: ${data.tracks.items[0].album.name}`);
+            console.log(`Preview URL: ${data.tracks.items[0].preview_url}`);
+            console.log("\n");
+            console.log("======================================================================");
+        })
+    }
 }
 
 function OMDB() {
     console.log("OMDB was called");
-
+    
+    if (query !== "") {
+        request(`http://www.omdbapi.com/?apikey=${keys.OMDB}&t=${query}`, function (err, response, body) {
+            console.log("======================================================================");
+            console.log("\n");
+            console.log(`Title: ${JSON.parse(body).Title}`);
+            console.log(`Year: ${JSON.parse(body).Year}`);
+            console.log(`IMDB Rating: ${JSON.parse(body).Ratings[0].Value}`);
+            console.log(`Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}`);
+            console.log(`Country: ${JSON.parse(body).Country}`);
+            console.log(`Language: ${JSON.parse(body).Language}`);
+            console.log(`Actors: ${JSON.parse(body).Actors}`);
+            console.log(`Plot: ${JSON.parse(body).Plot}`);
+            console.log("\n");
+            console.log("======================================================================");
+        })
+    }
+    else {
+        request(`http://www.omdbapi.com/?apikey=${keys.OMDB}&t=Mr Nobody`, function (err, response, body) {
+            console.log("======================================================================");
+            console.log("\n");
+            console.log(`Title: ${JSON.parse(body).Title}`);
+            console.log(`Year: ${JSON.parse(body).Year}`);
+            console.log(`IMDB Rating: ${JSON.parse(body).Ratings[0].Value}`);
+            console.log(`Rotten Tomatoes Rating: ${JSON.parse(body).Ratings[1].Value}`);
+            console.log(`Country: ${JSON.parse(body).Country}`);
+            console.log(`Language: ${JSON.parse(body).Language}`);
+            console.log(`Actors: ${JSON.parse(body).Actors}`);
+            console.log(`Plot: ${JSON.parse(body).Plot}`);
+            console.log("\n");
+            console.log("======================================================================");
+        })
+    }
+    
+    
 }
